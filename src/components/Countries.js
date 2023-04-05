7c6d342881db9b0213aa9be16e7cc05f8cbbaf3f
ORUCALIYEV <orucaliyefff@gmail.com>
2023-04-05 04:05:07 +0400

    done

---
+++ src/components/Countries.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import Filter from "./Filter"
+
+const url = "https://restcountries.com/v2/all"
+
+const Countries = () => {
+  const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch(url)
+      const Allcountries = await response.json()
+      setCountries(Allcountries)
+      setIsLoading(false)
+    }
+
+    fetchCountries()
+  }, [])
+
+  console.log(countries);
+
+  return (
+    <>
+      <Filter
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+        setFiltered={setFiltered}
+        setCountries={setCountries}
+        countries={countries}
+      />
+      {isLoading ? (
+        <h1 className="loading">Loading...</h1>
+      ) : searchInput.length > 1 ? (
+        <section className="countries">
+          {filtered.map((country) => {
+            const { numericCode, name, flag, population, region, capital } =
+              country
+
+            return (
+              <Link to={`/countries/${name}`} key={numericCode}>
+                <article>
+                  <div className="flag">
+                    <img src={flag} alt={name} />
+                  </div>
+                  <div className="details">
+                    <h4 className="country-name">
+                      Name: <span>{name}</span>
+                    </h4>
+                    <h4>
+                      Population: <span>{population.toLocaleString()}</span>
+                    </h4>
+                    <h4>
+                      Region: <span>{region}</span>
+                    </h4>
+                    <h4>
+                      Capital: <span>{capital}</span>
+                    </h4>
+                  </div>
+                </article>
+              </Link>
+            )
+          })}
+        </section>
+      ) : (
+        <section className="countries">
+          {countries.map((country) => {
+            const { numericCode, name, flag, population, region, capital } =
+              country
+
+            return (
+              <Link to={`/countries/${name}`} key={numericCode}>
+                <article>
+                  <div className="flag">
+                    <img src={flag} alt={name} />
+                  </div>
+                  <div className="details">
+                    <h4 className="country-name">
+                      Name: <span>{name}</span>
+                    </h4>
+                    <h4>
+                      Population: <span>{population.toLocaleString()}</span>
+                    </h4>
+                    <h4>
+                      Region: <span>{region}</span>
+                    </h4>
+                    <h4>
+                      Capital: <span>{capital}</span>
+                    </h4>
+                  </div>
+                </article>
+              </Link>
+            )
+          })}
+        </section>
+      )}
+    </>
+  )
+}
+
+export default Countries
---
+++ src/components/Filter.js
@@ -0,0 +1,103 @@
+import React, { useEffect } from "react";
+
+const Filter = ({
+  searchInput,
+  setSearchInput,
+  setFiltered,
+  setCountries,
+  countries,
+}) => {
+  const regions = [
+    {
+      name: "Filter by region",
+      desc: "All",
+    },
+    {
+      name: "Africa",
+      desc: "Africa",
+    },
+    {
+      name: "Americas",
+      desc: "Americas",
+    },
+    {
+      name: "Asia",
+      desc: "Asia",
+    },
+    {
+      name: "Europe",
+      desc: "Europe",
+    },
+    {
+      name: "Oceania",
+      desc: "Oceania",
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // Search countries
+  const searchCountries = (searchValue) => {
+    setSearchInput(searchValue);
+
+    if (searchInput) {
+      const filteredCountries = countries.filter((country) =>
+        Object.values(country)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFiltered(filteredCountries);
+    } else {
+      setFiltered(countries);
+    }
+  };
+
+
+
+  const filterRegions = async (region) => {
+    const url = `https://restcountries.com/v2/region/${region}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    filterRegions();
+
+  }, []);
+
+  return (
+    <>
+      <form className="form" id="form" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          autoComplete="off"
+          placeholder="Search Country"
+          onChange={(e) => searchCountries(e.target.value)}
+        />
+
+        <div className="select">
+          <select
+            name="select"
+            id="select"
+            onChange={(e) => filterRegions(e.target.value)}
+            value={regions.name}
+          >
+            <option value="Africa">Africa</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Americas">Americas</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Filter;
